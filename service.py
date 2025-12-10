0c6097fecd322fb1bb2de4981cfc26150d2eac61
Rohan Belsare <rohanbelsare113@gmail.com>
2025-12-10 23:02:09 +0530

    new changes

--- service.py
+++ service.py
@@ -18,7 +18,6 @@ def create_user(name,email,password):
     db.close()
     return {"message":"Values entered!!"}
 
-create_user()
 # results = create_user("Laukik", "laukikbelsare@gmail.com","Laukik@54321")
 # print(results)
 
@@ -36,8 +35,43 @@ def get_user_by_id(user_id):
         cursor.close()
         db.close()
 
-result2 = get_user_by_id(1)
-print(result2)
+# result2 = get_user_by_id(1)
+# print(result2)
 
 #write the function to update and delete the user
+
+def update_values(user_id,name,password,email):
+    try:
+        db=get_sql()
+        cursor=db.cursor(dictionary=True)
+        query="update tbluser set name = %s, email= %s, password= %s where id=%s "
+        cursor.execute(query,(name,email,password,user_id))
+        db.commit()
+        return {"message":"User updated succesfully!!"}
+    except Error as e:
+        return {"Error":str(e)}
+    finally:
+        db.close()
+        cursor.close()
+
+# result3=update_values(1,"Abhijeet","Abhijeet@54321","abhijeetmaurya@gmail.com")
+# print(result3)
+
+
+def delete_values(user_id):
+    try:
+        db=get_sql()
+        cursor=db.cursor(dictionary=True)
+        query= "delete from tbluser where id=%s "
+        cursor.execute(query,(user_id,))
+        db.commit()
+        return {f"message":"row with id: {usere_id} deleted!! "}
+    except Error as e:
+        return {"Error":str(e)}
+    finally:
+        db.close()
+        cursor.close()
+
+result4=delete_values(1)
+print(result4)
 
